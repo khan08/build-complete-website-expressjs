8e05376766d2c22ab7ffd08a38ac0af3450bbb9c
kang <kang_han@caslservice.org>
2017-09-13 18:02:32 -0500

    mysql migrate

--- app/app.js
+++ app/app.js
@@ -8,11 +8,12 @@ var express = require('express'),
 	path = require('path'),
 	config = require('./config')(),
 	app = express(),
-	MongoClient = require('mongodb').MongoClient,
+	mysql = require('mysql'),
 	Admin = require('./controllers/Admin'),
 	Home = require('./controllers/Home'),
 	Blog = require('./controllers/Blog'),
 	Page = require('./controllers/Page');
+	db = require('./config/db')
 
 // all environments
 // app.set('port', process.env.PORT || 3000);
@@ -33,40 +34,34 @@ if ('development' == app.get('env')) {
   	app.use(express.errorHandler());
 }
 
-MongoClient.connect('mongodb://' + config.mongo.host + ':' + config.mongo.port + '/fastdelivery', function(err, db) {
-	if(err) {
-		console.log('Sorry, there is no mongo db server running.');
-	} else {
-		var attachDB = function(req, res, next) {
-			req.db = db;
-			next();
-		};
-		app.all('/admin*', attachDB, function(req, res, next) {
-			Admin.run(req, res, next);
-		});			
-		app.all('/blog/:id', attachDB, function(req, res, next) {
-			Blog.runArticle(req, res, next);
-		});	
-		app.all('/blog', attachDB, function(req, res, next) {
-			Blog.run(req, res, next);
-		});	
-		app.all('/services', attachDB, function(req, res, next) {
-			Page.run('services', req, res, next);
-		});	
-		app.all('/careers', attachDB, function(req, res, next) {
-			Page.run('careers', req, res, next);
-		});	
-		app.all('/contacts', attachDB, function(req, res, next) {
-			Page.run('contacts', req, res, next);
-		});	
-		app.all('/', attachDB, function(req, res, next) {
-			Home.run(req, res, next);
-		});		
-		http.createServer(app).listen(config.port, function() {
-		  	console.log(
-		  		'Successfully connected to mongodb://' + config.mongo.host + ':' + config.mongo.port,
-		  		'\nExpress server listening on port ' + config.port
-		  	);
-		});
-	}
+var attachDB = function(req, res, next) {
+	req.db = db;
+	next();
+};
+app.all('/admin*', attachDB, function(req, res, next) {
+	Admin.run(req, res, next);
+});			
+app.all('/blog/:id', attachDB, function(req, res, next) {
+	Blog.runArticle(req, res, next);
+});	
+app.all('/blog', attachDB, function(req, res, next) {
+	Blog.run(req, res, next);
+});	
+app.all('/services', attachDB, function(req, res, next) {
+	Page.run('services', req, res, next);
+});	
+app.all('/careers', attachDB, function(req, res, next) {
+	Page.run('careers', req, res, next);
+});	
+app.all('/contacts', attachDB, function(req, res, next) {
+	Page.run('contacts', req, res, next);
+});	
+app.all('/', attachDB, function(req, res, next) {
+	Home.run(req, res, next);
+});		
+http.createServer(app).listen(config.port, function() {
+	console.log(
+		'Successfully connected to mongodb://' + config.mongo.host + ':' + config.mongo.port,
+		'\nExpress server listening on port ' + config.port
+	);
 });
